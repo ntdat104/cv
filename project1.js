052a37cadc523fdea8d1cef890f8502dbb75682c
Tiến Đạt <47631594+ntdat104@users.noreply.github.com>
2019-12-31 23:19:46 +0700

    update

---
+++ project1.js
@@ -0,0 +1,5 @@
+const cursor = document.querySelector(".cursor");
+    
+document.addEventListener("mousemove", e => {
+    cursor.setAttribute("style", "top: "+(e.pageY-8)+"px; left: "+(e.pageX-8)+"px;")
+})
