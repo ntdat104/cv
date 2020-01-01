b3e8152091d12a87202d2cb252cf7a9d6cfe372f
Tiến Đạt <47631594+ntdat104@users.noreply.github.com>
2020-01-01 20:26:35 +0700

    update

--- main.js
+++
@@ -1,5 +0,0 @@
-const cursor = document.querySelector(".cursor");
-
-document.addEventListener("mousemove", e => {
-    cursor.setAttribute("style", "top: "+(e.pageY-8)+"px; left: "+(e.pageX-8)+"px;")
-})
