7e89c399722ba29e711333e1afaa872d498699a2
Tiến Đạt <47631594+ntdat104@users.noreply.github.com>
2020-04-14 09:44:15 +0700

    update

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 let mouseCursor = document.querySelector(".cursor");
-    
-window.addEventListener("mousemove",cursor);
-
-function cursor(e){
-    mouseCursor.style.top = e.pageY - 12 + "px";
-    mouseCursor.style.left = e.pageX - 12 + "px";
+window.addEventListener("mousemove", cursor);
+function cursor(e) {
+  mouseCursor.style.top = e.pageY - 12 + "px";
+  mouseCursor.style.left = e.pageX - 12 + "px";
 }
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").style.top = "0";
+  } else {
+    document.getElementById("header").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+};
